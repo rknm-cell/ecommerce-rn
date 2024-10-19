ff83b0b173b2764292bbb2a39f3991c3f997427e
rknm-cell <racknahm@gmail.com>
2024-10-19 19:13:37 -0400

    added modal

--- components/ImageViewer.tsx
+++ components/ImageViewer.tsx
@@ -3,12 +3,13 @@ import { Image } from "expo-image";
 
 type Props = {
     imgSource: string;
+    selectedImage?: string;
 }
 
-export default function ImageViewer({ imgSource }: Props) {
-    console.log(imgSource)
+export default function ImageViewer({ imgSource, selectedImage }: Props) {
+    const imageSource = selectedImage ? {uri: selectedImage} : imgSource;
     return (
-    <Image source={imgSource} style={styles.image} />
+    <Image source={imageSource} style={styles.image} />
   
 )    ;
 }
